320b8b573b1e754246194eb8b115f24d55736972
Oragif <Mikkelhelsing1@gmail.com>
2022-11-22 22:18:59 +0100

    Added str replace function

--- string_util.c
+++ string_util.c
@@ -1,6 +1,8 @@
 #include "string_util.h"
 #include <string.h>
 #include <stdbool.h>
+#include <malloc.h>
+#include <stdio.h>
 //Should be 32, but just in case
 #define ANSI_CASE_DIF 'a' - 'A'
 #define NOTFOUND -1
@@ -8,6 +10,7 @@
 //Prototypes for utility functions
 _Bool char_is_upper(char _char);
 _Bool char_is_lower(char _char);
+void str_offset_index(char **string, int index, int offset);
 
 /* =============================================== *
  *         Lower- & uppercase conversion           *
@@ -46,7 +49,7 @@ void str_to_upper(char **string) {
 }
 
 /* =============================================== *
- *         Lower- & uppercase conversion           *
+ *       Contains char or string functions         *
  * =============================================== */
 
 /**
@@ -67,26 +70,109 @@ int str_contains_char(const char* string, char c) {
 /**
  * Looks for a string in a string
  * @param string char*, string to search
- * @param str_to_find char*, string to find
+ * @param to_find char*, string to find
  * @param case_sensitive _Bool, whether to ignore case
  * @return index of first letter if found, otherwise -1
  */
-int str_contains_str(char* string, char* str_to_find, _Bool case_sensitive) {
+int str_contains_str(char* string, char* to_find, _Bool case_sensitive) {
     if (!case_sensitive) {
         str_to_lower(&string);
-        str_to_lower(&str_to_find);
+        str_to_lower(&to_find);
     }
     //Find the first char in string
-    int first_index = str_contains_char(string, str_to_find[0]);
+    int first_index = str_contains_char(string, to_find[0]);
     if (first_index != NOTFOUND) {
         int count = 1;
         //Keep checking until either full string found, out of bounds on either strings or character didn't match
-        for (int j = 1; str_to_find[j] != '\0' && string[first_index + j] != '\0' && str_to_find[j] == string[first_index + j]; ++j)
+        for (int j = 1; to_find[j] != '\0' && string[first_index + j] != '\0' && to_find[j] == string[first_index + j]; ++j)
             ++count;
         //Check if a string match was found, if so return index of first char
-        if (count == strlen(str_to_find)) return first_index;
+        if (count == strlen(to_find)) return first_index;
     }
     return NOTFOUND;
+}
+
+/* =============================================== *
+ *        Replace string and char functions        *
+ * =============================================== */
+
+/**
+ * @brief Replaces all instances of a string\n
+ * If size changes the string will be reallocated to match\n
+ * !! Remember to set deallocate if the inputted string is in heap !!
+ * @param string char*, string to replace in
+ * @param to_find char*, string to replace
+ * @param to_replace char*, string to replace with
+ * @param case_sensitive _Bool, whether to ignore case
+ * @param deallocate _Bool, whether to deallocate the inputted string if size changes
+ * @return
+ */
+char* str_replace_str(char *string, char* to_find, char* to_replace, _Bool case_sensitive, _Bool deallocate) {
+    char* ret_str;
+    _Bool free_ret_str = false;
+    int to_replace_len = (int) strlen(to_replace),
+        to_find_len    = (int) strlen(to_find);
+
+    //If replacing word is bigger, then what it's replacing more memory is needed to do replacement
+    if (to_find_len < to_replace_len) {
+        //Calculate the maximum size, meaning if every word is replaced
+        size_t max_str_size = (strlen(string)/ to_find_len) * to_replace_len + 1;
+        //Allocate and copy original string
+        ret_str = (char*) malloc( max_str_size * sizeof(char));
+        strcpy(ret_str, string);
+        free_ret_str = true;
+    } else {
+        //Otherwise duplicate the string to avoid memory errors
+        ret_str = strdup(string);
+    }
+
+    int index = 0,
+        string_offset = 0,
+        index_offset  = to_replace_len - to_find_len + 1;
+    _Bool changed = false;
+    //Loop as long as words is being found
+    do {
+        //Try to find word
+        // !!!!!! Index fucked, plz fix !!!!!!
+        char* next =  &ret_str[index + string_offset];
+        index = str_contains_str(next, to_find, case_sensitive);
+        printf("%d + %d = %d\n", index, string_offset, index + string_offset);
+        if (index != NOTFOUND) {
+            //Offset string so new word fits
+            str_offset_index(&ret_str, index, index_offset);
+            //Replace character with the replacing word
+            for (int i = 0; i < to_replace_len; ++i)
+                ret_str[index + i] = to_replace[i];
+            //Count the total offset added
+            string_offset += index_offset;
+            changed = true;
+        }
+    } while (index != NOTFOUND);
+
+    //If anything has changed update string
+    if (changed) {
+        //Create a stop to string encase more memory has been allocated
+        int new_size = string_offset + (int) strlen(string);
+        ret_str[new_size] = '\0';
+
+        //If the two words are equal update the string and return
+        if (to_replace_len == to_find_len) {
+            strcpy(string, ret_str);
+            return string;
+        } else {
+            //Otherwise create new allocation with correct size and set it to the replaced string
+            char* retPtr = (char*) malloc(new_size * sizeof(char ));
+            strcpy(retPtr, ret_str);
+            //Free if allocated
+            if (free_ret_str) free(ret_str);
+            if (deallocate)   free(string);
+            return retPtr;
+        }
+    }
+
+    if (free_ret_str) free(ret_str);
+
+    return string;
 }
 
 /* =============================================== *
@@ -113,4 +199,25 @@ _Bool char_is_lower(char _char) {
     if (_char >= 'a' && _char <= 'z')
         return true;
     return false;
+}
+
+/**
+ * @brief Moves part of a string by an offset\n
+ * !! Make sure enough space is allocated for new string !!
+ * @param string char**, string to alter
+ * @param index int, index of split, everything after will be moved by the offset
+ * @param offset int, how much offset after split
+ */
+void str_offset_index(char **string, int index, int offset) {
+    if (offset == 0) return;
+
+    char* ret_str = strdup(*string);
+    char* offset_str = strdup(&ret_str[index]);
+
+    ret_str[index + offset] = '\0';
+    for (int i = index; i < (index + offset); ++i) {
+        ret_str[i] = ' ';
+    }
+    strcat(ret_str, offset_str);
+    *string = ret_str;
 }
--- string_util.h
+++ string_util.h
@@ -3,6 +3,7 @@
 #include <string.h>
 void str_to_lower(char **string);
 void str_to_upper(char **string);
-int str_contains_str(char* string, char* str_to_find, _Bool case_sensitive);
+int str_contains_str(char* string, char* to_find, _Bool case_sensitive);
 int str_contains_char(const char* string, char c);
+char* str_replace_str(char *string, char* to_find, char* to_replace, _Bool case_sensitive, _Bool deallocate);
 #endif //STRING_UTIL_STRING_UTIL_H
